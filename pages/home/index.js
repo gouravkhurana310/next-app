058eed9ceb3407fe51606cf1fb409420068653aa
Khurana <gourav.khurana@payucredits.com>
2022-01-11 23:06:11 +0530

    publish sbox

---
+++ pages/home/index.js
@@ -0,0 +1,4 @@
+function App(){
+    return <h2>Hello world this is home page </h2>
+}
+export default App;
---
+++ pages/page1/index.js
@@ -0,0 +1,4 @@
+function App(){
+    return <h2>This seems to be page one.</h2>
+}
+export default App;
